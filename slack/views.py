df3eb7b494c63aac07fefe3761c99d954f1e46ce
Mike Jewell <mjewell@gmail.com>
2015-05-19 17:34:01 +0100

    Add csrf exemption

--- slack/views.py
+++ slack/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseForbidden, HttpResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def bookmark(request):
 	return HttpResponse("Done!")
