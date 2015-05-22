8f4be849e3d4ca78d9eab522196748abf832f80e
Mike Jewell <mjewell@gmail.com>
2015-05-22 17:45:58 +0100

    Removed tests

--- jukebox/urls.py
+++ jukebox/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^test$', views.test, name='test'),
     url(r'^authorise$', views.authorise, name='authorise'),
     url(r'^callback$', views.callback, name='callback'),
 ]
