686d2f77b565b6235554fed46d4bcc102014d906
Mike Jewell <mjewell@gmail.com>
2015-05-22 17:35:31 +0100

    boto

--- slack/settings.py
+++ slack/settings.py
@@ -45,7 +45,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # Application definition
 
