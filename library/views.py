c3f5faf1415c3b7a4d1028a4ce767086499a485f
Mike Jewell <mjewell@gmail.com>
2015-05-26 20:38:03 +0100

    Small tweak to handle single books (which don't come back as a list)

--- library/views.py
+++ library/views.py
@@ -118,6 +118,8 @@ def index(request):
 					response += "Books with @"+shelf_name+":\n"
 					resp = gc.request("review/list.xml", {'page':1,'format':'xml', 'id':settings.GOODREADS_USERID, 'shelf':shelf_name})
 					books = resp['books']['book']
+					if type(books) != list:
+						books = [books]
 					for book in books:
 						response += "*"+book['title']+" - "+book['authors']['author']['name']+"* ("+book['isbn']+")\n"
 			else:
